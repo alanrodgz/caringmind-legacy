c4b28caed6581a75b81c1cd68a6f60a364f2bbdd
Alan Rodriguez <alanrodriguez102347@gmail.com>
2024-11-01 17:06:22 -0700

    Initial Commit

---
+++ caringmind/caringmindApp.swift
@@ -0,0 +1,17 @@
+//
+//  caringmindApp.swift
+//  caringmind
+//
+//  Created by Alan on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct caringmindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
